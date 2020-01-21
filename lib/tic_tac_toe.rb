da42436fc4d3fd92d17938f3f8424f6e0837991e
Ricky dildy <rdildy92@gmail.com>
2020-01-21 22:27:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,58 +1,7 @@
 class TicTacToe
 
   def initialize(board = nil)
-    @board = board || Array.new(9, " ")
-  end
-end 
-
-def play
-    @board = Array.new(9, " ")
-    
-  
-  def turn
-    puts "Player #{current_player}, please enter a number 1-9:"
-    input = gets.strip
-    index = input_to_index(input)
-    cp = current_player
-    if valid_move?(index)
-      move(index, cp)
-      display_board
-    else
-      turn
-    end
-  end
-
-  def input_to_index(input)
-    input.to_i - 1
-  end
-
-  def valid_move?(index)
-    index.between?(0,8) && !position_taken?(index)
-  end
-
-  def position_taken?(index)
-    !(@board[index].nil? || @board[index] == " ")
-  end
-
-  def move(index, token = "X")
-    @board[index] = token
-  end
-
-  def current_player
-    turn_count % 2 == 0 ? "X" : "O"
-  end
-
-  def turn_count
-    @board.count{|token| token == "X" || token == "O"}
-  end
-
-  def display_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-  end
+    @board = board || Arra
 end
 
 
